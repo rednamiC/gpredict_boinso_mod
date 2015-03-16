71a6983879a91ec622f5b2d3033cf16aaa0eeb04
Marcel C <rednamic@rednamic-Aspire-V7-582PG>
2015-03-16 15:37:14 +0100

    first beta of the gpredict boinso client. Rot-/Rigcontroll track selected satellites until LOS and switch to next satellite automatically

---
+++ src/next_sat_head_mc.h
@@ -0,0 +1,9 @@
+//MC
+
+//MC nextsat to pass to gtk-rot-ctrl.c and gtk-rig-ctrl.c
+extern char **satlist_mc_nick;
+extern char next_sat_mc[256];
+extern char active_target_nick[256];
+extern int target_aquired;
+
+
